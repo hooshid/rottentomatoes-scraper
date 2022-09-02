d5932025ba31868c3a28356589ae40a3b3bcddd5
Alireza Kamali <arka1373@gmail.com>
2022-09-02 08:32:53 +0430

    Add headers for requests

--- src/Base/Base.php
+++ src/Base/Base.php
@@ -17,7 +17,15 @@ class Base
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_ENCODING, "");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+        curl_setopt($ch, CURLOPT_REFERER, "https://www.rottentomatoes.com/");
+
+        $requestHeaders = array();
+        $requestHeaders[] = "accept: */*";
+        $requestHeaders[] = "accept-encoding: gzip, deflate, br";
+        $requestHeaders[] = "accept-language: en-US,en;";
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $requestHeaders);
+
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4472.124 Safari/537.36");
         $page = curl_exec($ch);
         curl_close($ch);
 
